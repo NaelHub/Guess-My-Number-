dad16c96716aec2e553551e7bafacbb15f0145ce
Nael Hubaishi <nael.hub@gmail.com>
2022-11-14 23:29:18 -0500

    Implementing Game Logic - Score Counter etc

--- script.js
+++ script.js
@@ -9,12 +9,39 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+// value of random number for game
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      // game logic implemented with number and feedback msg
+      // decreasing score implemented as well
+    }
   }
 });
 // added functionality to the "Check" button.
